b004e1d5d8aab09279c2ea6cb7026a0d0c7595dd
M-Quinn <47506506+M-Quinn@users.noreply.github.com>
2022-10-13 11:26:51 -0400

    feat: add ability bunny now wanders when full

--- Assets/Scripts/Bunny.cs
+++ Assets/Scripts/Bunny.cs
@@ -30,11 +30,19 @@ public class Bunny : MonoBehaviour
         {
             FindFood(transform.position, 25f, _foodMask);
         }
-        else if (1 == 2) { }
-        //if found food
-            //eat -> reproduce
-
-        
+        else if (_wanderPosition == null)
+        {
+            GetWanderPosition();
+        }
+        else if (_wanderPosition != null) {
+            var direction = _wanderPosition - transform.position;
+            direction.Normalize();
+            transform.Translate(direction * _speed * Time.deltaTime);
+            //If reached destination, get new destination
+            if (Vector3.Distance(transform.position, _wanderPosition) <= 1) {
+                GetWanderPosition();
+            }
+        }
     }
 
     private void FindFood(Vector3 center, float radius, LayerMask foodMask)
@@ -47,17 +55,13 @@ public class Bunny : MonoBehaviour
         }
     }
 
-    private void Wander() { 
-    
+    private void GetWanderPosition() {
+        _wanderPosition = new Vector3(Random.Range(-49, 50), 0, Random.Range(-49, 50));
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.transform.CompareTag("Wolf"))
-        {
-            //take hit
-        }
-        else if (collision.transform.CompareTag("Food")) {
+        if (collision.transform.CompareTag("Food")) {
             _hunger.Eat();
             Destroy(collision.gameObject);
             Debug.Log("Collided with food");
